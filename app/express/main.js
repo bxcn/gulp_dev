9b5ecb1f278ec62ef961f36c6e1305c4287aae79
海水 <hi_beijing@126.com>
2016-10-31 13:30:51 +0800

    add express jade ejs 配置文件

---
+++ app/express/main.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+//利用Express托管静态文件
+app.use(express.static('Public'));
+app.use(express.static('html'));
+
+app.set('views', './views')
+app.set('view engine', 'ejs');
+
+app.get('/index', function (req, res) {
+  //res.send('Hello World!/static');
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+
+// 没有挂载路径的中间件，应用的每个请求都会执行该中间件
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+// 一个中间件栈，对任何指向 /user/:id 的 HTTP 请求打印出相关信息
+app.use('/user/:id', function(req, res, next) {
+  console.log('Request URL:', req.originalUrl);
+  next();
+}, function (req, res, next) {
+  console.log('Request Type:', req.method);
+  next();
+});
+
+// 一个中间件栈，处理指向 /user/:id 的 GET 请求
+app.get('/user/:id', function (req, res, next) {
+  console.log('ID:', req.params.id);
+  next();
+}, function (req, res, next) {
+  res.send('User Info');
+});
+
+// 处理 /user/:id， 打印出用户 id
+app.get('/user/:id', function (req, res, next) {
+  res.end(req.params.id);
+});
+
+
+const birds = require('./birds');
+app.use("/birds",birds);
+
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+/*
+路由是指如何定义应用的端点（URIs）以及如何响应客户端的请求。
+路由是由一个 URI、HTTP 请求（GET、POST等）和若干个句柄组成，
+它的结构如下： app.METHOD(path, [callback...], callback)， 
+app 是 express 对象的一个实例， METHOD 是一个 HTTP 请求方法， 
+path 是服务器上的路径， callback 是当路由匹配时要执行的函数。
+*/
