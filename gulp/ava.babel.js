619448d97f40fedb1a1317bffb2048946dc256cd
海水 <hi_beijing@126.com>
2016-11-29 19:04:38 +0800

    add

--- gulp/ava.babel.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-
-gulp.task('mocha', () =>
-  gulp.src('mocha/**/*.test.js')
-  // gulp-mocha needs filepaths so you can't have any plugins before it 
-  .pipe(mocha())
-);
